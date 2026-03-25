8cc49283c2c401c252f0f0d97687a3475cb2c280
3milioAz <myemivalor@gmail.com>
2026-03-25 08:09:24 -0600

    Version 4 (Instancia Doble objeto 1)

--- src/main/java/unidad2/sensorIA/Main.java
+++ src/main/java/unidad2/sensorIA/Main.java
@@ -18,12 +18,16 @@ public class Main {
         SensorUltrasonido obj5 = new SensorUltrasonido("Modelo 5", 1.21);
         SensorUltrasonido obj6 = new SensorUltrasonido("Modelo 6", 1.35);
 
+
         percepcionHub.add(obj1);
         percepcionHub.add(obj2);
         percepcionHub.add(obj3);
         percepcionHub.add(obj4);
         percepcionHub.add(obj5);
         percepcionHub.add(obj6);
+
+        obj1 = new SensorLidar("Modelo 7", 1.87);
+        percepcionHub.add(obj1);
 
         for (SensorIA f: percepcionHub){
             f.leerDatos();
